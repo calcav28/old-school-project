1e23bca2e5377b59c80c1dee389c3ed740d55fa5
calcav28 <cavanagh.calvin@gmail.com>
2022-11-16 19:17:23 -0800

    Phase 3 First Frame Complete

--- src/main/ui/GameApp.java
+++ src/main/ui/GameApp.java
@@ -37,6 +37,9 @@ public class GameApp extends JPanel implements ActionListener {
     private JTextField playerNameInput;
     private JTextField playerNumberInput;
 
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 1000;
+
     //Starts the player at a title screen
     @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
     public GameApp() {
@@ -54,51 +57,48 @@ public class GameApp extends JPanel implements ActionListener {
         questionLabel.setVerticalAlignment(JLabel.TOP);
 
         JPanel questionPanel = new JPanel();
-        questionPanel.setSize(100, 100);
-        questionPanel.setAlignmentX(200);
-        questionPanel.setAlignmentY(200);
+        questionPanel.setSize(WIDTH / 2, HEIGHT / 2);
+        questionPanel.setAlignmentX(WIDTH / 2);
+        questionPanel.setAlignmentY(HEIGHT / 2);
         questionPanel.add(questionLabel);
 
-        JButton btn1 = new JButton("1. Start New Game");
+        JButton btn1 = new JButton("Start New Game");
         btn1.addActionListener(this);
         btn1.setHorizontalAlignment(JLabel.CENTER);
         btn1.setVerticalAlignment(JLabel.CENTER);
-        JButton btn2 = new JButton("2. Load Saved Game");
-        btn2.setBounds(90, 90, 10, 10);
+        JButton btn2 = new JButton("Load Saved Game");
         btn2.addActionListener(this);
         btn2.setHorizontalAlignment(JLabel.CENTER);
         btn2.setVerticalAlignment(JLabel.CENTER);
-        JButton btn3 = new JButton("3. Close Game");
-        btn3.setBounds(80, 80, 10, 10);
+        JButton btn3 = new JButton("Close Game");
         btn3.addActionListener(this);
         btn3.setHorizontalAlignment(JLabel.CENTER);
         btn3.setVerticalAlignment(JLabel.CENTER);
 
         JPanel buttonFrame1 = new JPanel();
-        buttonFrame1.setSize(100, 100);
+        buttonFrame1.setSize(200, 100);
         buttonFrame1.add(btn1);
 
         JPanel buttonFrame2 = new JPanel();
-        buttonFrame2.setSize(100, 100);
+        buttonFrame2.setSize(200, 100);
         buttonFrame2.add(btn2);
 
         JPanel buttonFrame3 = new JPanel();
-        buttonFrame3.setSize(100, 100);
+        buttonFrame3.setSize(200, 100);
         buttonFrame3.add(btn3);
 
         JFrame frame = new JFrame();
-        frame.setSize(500, 500);
+        frame.setSize(WIDTH, HEIGHT);
         frame.setTitle("Welcome to the game!");
         frame.setResizable(false);
         btn1.setActionCommand("TitleChoice1");
         btn2.setActionCommand("TitleChoice2");
         btn3.setActionCommand("TitleChoice3");
-        frame.add(questionPanel);
-        btn1.setHorizontalAlignment(JLabel.CENTER);
-        btn1.setVerticalAlignment(JLabel.CENTER);
-        frame.add(buttonFrame1, BorderLayout.NORTH);
-        frame.add(buttonFrame2, BorderLayout.CENTER);
-        frame.add(buttonFrame3, BorderLayout.SOUTH);
+        frame.add(questionPanel, BorderLayout.NORTH);
+        questionPanel.setLocation(WIDTH / 2,HEIGHT - HEIGHT);
+        frame.add(buttonFrame1, BorderLayout.CENTER);
+        frame.add(buttonFrame2, BorderLayout.WEST);
+        frame.add(buttonFrame3, BorderLayout.EAST);
         frame.setVisible(true);
     }
 
@@ -141,54 +141,55 @@ public class GameApp extends JPanel implements ActionListener {
         game.setProgress(stage);
 
         JFrame frame = new JFrame();
-        frame.setSize(1000, 1000);
-
+        frame.setSize(WIDTH, HEIGHT);
 
         JLabel label1 = new JLabel();
         label1.setText("You are inside a spaceship. The captain wants you to get rid of our cargo.");
+        label1.setSize(25, 25);
         label1.setHorizontalTextPosition(SwingConstants.CENTER);
 
         JLabel label2 = new JLabel();
         label2.setText("The Captain said that the cargo is to the right. Which way do you go?");
+        label2.setSize(25, 25);
+        label2.setHorizontalTextPosition(SwingConstants.CENTER);
 
         JPanel labelPanel = new JPanel();
-        labelPanel.setSize(600, 600);
-        labelPanel.setLocation(500, 50);
+        labelPanel.setSize(WIDTH / 2, HEIGHT / 2);
+        labelPanel.setLocation(100, 50);
         labelPanel.add(label1);
-        //labelPanel.add(label2);
+        labelPanel.add(label2);
 
 
         JButton btn1 = new JButton("1. Right");
         btn1.addActionListener(this);
         btn1.setActionCommand("p1s1c0");
         JPanel btn1Panel = new JPanel();
-        btn1Panel.setSize(600, 100);
-        btn1Panel.setLocation(100,400);
+        btn1Panel.setSize(WIDTH / 10, HEIGHT / 10);
+        btn1Panel.setLocation(100,100);
         btn1Panel.add(btn1);
-        btn1Panel.add(label1);
 
         JButton btn2 = new JButton("2. Left");
         btn2.addActionListener(this);
         btn2.setActionCommand("p2s1c0");
         JPanel btn2Panel = new JPanel();
         btn2Panel.setSize(100, 100);
-        btn2Panel.setLocation(100,500);
+        btn2Panel.setLocation(100,200);
         btn2Panel.add(btn2);
 
         JButton btn3 = new JButton("3. Save Game");
         btn3.addActionListener(this);
         btn3.setActionCommand("saveGame");
         JPanel btn3Panel = new JPanel();
-        btn3Panel.setSize(100, 100);
-        btn3Panel.setLocation(300,600);
+        btn3Panel.setSize(120, 100);
+        btn3Panel.setLocation(100,300);
         btn3Panel.add(btn3);
 
         JButton btn4 = new JButton("4. Go Back to Title Screen");
         btn4.addActionListener(this);
         btn4.setActionCommand("titleScreen");
         JPanel btn4Panel = new JPanel();
-        btn4Panel.setSize(100, 100);
-        btn4Panel.setLocation(400,700);
+        btn4Panel.setSize(300, 100);
+        btn4Panel.setLocation(100,400);
         btn4Panel.add(btn4);
 
         frame.add(labelPanel);
@@ -206,6 +207,9 @@ public class GameApp extends JPanel implements ActionListener {
         input = new Scanner(System.in);
         stage = "p1s1c0";
         game.setProgress(stage);
+
+
+
 
         System.out.println("As you enter the room, you see a box of cheese slices. Do you take one?");
         System.out.println("1. Take a cheese slice.");
